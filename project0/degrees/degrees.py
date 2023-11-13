ced6e3c0860e796b52cea0fadfb9f47986d36ac0
Nate Andrews <andrewsnate94@gmail.com>
2023-11-13 18:08:21 -0500

    finished degrees

--- project0/degrees/degrees.py
+++ project0/degrees/degrees.py
@@ -91,21 +91,56 @@ def shortest_path(source, target):
 
     If no possible path, returns None.
     """
-    source_id = person_id_for_name(source)
-    target_id = person_id_for_name(target)
-    explored = set()
+    
 
-    start = Node(movie_id=None, person_id=source_id)
+# Keep track of number of states explored
+    num_explored = 0
+
+    # Initialize frontier to just the starting position
+    start = Node(state=source, parent=None, action=None)
     frontier = QueueFrontier()
     frontier.add(start)
-    
+
+    # Initialize an empty explored set
+    explored = set()
+
+    # Keep looping until solution found
     while True:
+
+        # If nothing left in frontier, then no path
         if frontier.empty():
             return None
 
+        # Choose a node from the frontier
+        node = frontier.remove()
+        if node.state == target:
+            path = []
+            while node.parent is not None:
+                path.append((node.action, node.state))
+                node = node.parent
+            path.reverse()
+            return path
 
-    # TODO
-    raise NotImplementedError
+        # If node is the goal, then we have a solution
+
+
+        # Mark node as explored
+        explored.add(node.state)
+
+        neighbors = neighbors_for_person(node.state)
+
+        for action, state in neighbors:
+            if not frontier.contains_state(state) and state not in explored:
+                child = Node(state=state, parent=node, action=action)
+                if child.state == target:
+                    path = []
+                    while child.parent is not None:
+                        path.append((child.action, child.state))
+                        child = child.parent
+
+                    path.reverse()
+                    return path
+                frontier.add(child)
 
 
 def person_id_for_name(name):
